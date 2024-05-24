ed1b42a2f85c6b622709947af092844c113c0876
Ankit Kumar <ankitkr8540@gmail.com>
2024-05-24 10:43:13 -0400

    blogs initialize

--- src/components/Blogs/blog.js
+++ src/components/Blogs/blog.js
@@ -1,13 +1,18 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
+import Particle from "../Particle";
+
 
 function blog() {
     return (
-        <section>
-            <Container className="home-section">
-                <h1>Blog</h1>
+        <Container fluid className="project-section">
+            <Particle />
+            <Container>
+                <h1 className="project-heading">
+                    <strong className="purple">Blogs coming soon ! </strong>
+                </h1>
             </Container>
-        </section>
+        </Container>
     )
 }
 
