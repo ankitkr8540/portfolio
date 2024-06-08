6c0cba20315e4af3ddaef402422cea237005ca62
Ankit Kumar <ankitkr8540@gmail.com>
2024-06-08 09:20:08 -0400

    small changes done

--- src/components/Blogs/blog.js
+++ src/components/Blogs/blog.js
@@ -1,19 +1,18 @@
 import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
-import Particle from "../Particle";
-
+import { Container } from 'react-bootstrap'
+import Particle from '../Particle'
 
 function blog() {
-    return (
-        <Container fluid className="project-section">
-            <Particle />
-            <Container>
-                <h1 className="project-heading">
-                    <strong className="purple">Blogs coming soon ! </strong>
-                </h1>
-            </Container>
-        </Container>
-    )
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          <strong className="purple">Blogs coming soon ! </strong>
+        </h1>
+      </Container>
+    </Container>
+  )
 }
 
 export default blog
