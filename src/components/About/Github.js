0889c16e40caa2ed10ea23c9d8bb50f86ccd57e9
Ankit Kumar <ankitkr8540@gmail.com>
2024-06-12 22:26:27 -0400

    added Blog link

--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -1,22 +1,25 @@
-import React from "react";
-import GitHubCalendar from "react-github-calendar";
-import { Row } from "react-bootstrap";
+import React from 'react'
+import GitHubCalendar from 'react-github-calendar'
+import { Row } from 'react-bootstrap'
+import LeetCodeCalendar from 'leet-calendar'
 
 function Github() {
   return (
-    <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-      <h1 className="project-heading" style={{ paddingBottom: "20px" }}>
-        Days I <strong className="purple">Code</strong>
-      </h1>
-      <GitHubCalendar
-        username="ankitkr8540"
-        blockSize={15}
-        blockMargin={5}
-        color="#c084f5"
-        fontSize={16}
-      />
-    </Row>
-  );
+    <>
+      <Row style={{ justifyContent: 'center', paddingBottom: '10px' }}>
+        <h1 className="project-heading" style={{ paddingBottom: '20px' }}>
+          Days I <strong className="purple">Code</strong>
+        </h1>
+        <GitHubCalendar username="ankitkr8540" blockSize={15} blockMargin={5} color="#c084f5" fontSize={16} />
+      </Row>
+      {/* <Row style={{ justifyContent: 'center', paddingBottom: '10px' }}>
+        <h1 className="project-heading" style={{ paddingBottom: '20px' }}>
+          Days I <strong className="purple">LeetCode</strong>
+        </h1>
+        <LeetCodeCalendar username="ankitkr8540" />
+      </Row> */}
+    </>
+  )
 }
 
-export default Github;
+export default Github
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,38 +1,30 @@
-import React, { useState } from "react";
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
-import Container from "react-bootstrap/Container";
-import logo from "../Assets/developer.png";
-import { Link } from "react-router-dom";
-import { ImBlog } from "react-icons/im";
-import {
-  AiOutlineFundProjectionScreen,
-  AiOutlineUser,
-} from "react-icons/ai";
+import React, { useState } from 'react'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Container from 'react-bootstrap/Container'
+import logo from '../Assets/developer.png'
+import { Link } from 'react-router-dom'
+import { ImBlog } from 'react-icons/im'
+import { AiOutlineFundProjectionScreen, AiOutlineUser } from 'react-icons/ai'
 
-import { CgFileDocument } from "react-icons/cg";
+import { CgFileDocument } from 'react-icons/cg'
 
 function NavBar() {
-  const [expand, updateExpanded] = useState(false);
-  const [navColour, updateNavbar] = useState(false);
+  const [expand, updateExpanded] = useState(false)
+  const [navColour, updateNavbar] = useState(false)
 
   function scrollHandler() {
     if (window.scrollY >= 20) {
-      updateNavbar(true);
+      updateNavbar(true)
     } else {
-      updateNavbar(false);
+      updateNavbar(false)
     }
   }
 
-  window.addEventListener("scroll", scrollHandler);
+  window.addEventListener('scroll', scrollHandler)
 
   return (
-    <Navbar
-      expanded={expand}
-      fixed="top"
-      expand="md"
-      className={navColour ? "sticky" : "navbar"}
-    >
+    <Navbar expanded={expand} fixed="top" expand="md" className={navColour ? 'sticky' : 'navbar'}>
       <Container>
         <Navbar.Brand as={Link} to="/" className="d-flex">
           <img src={logo} className="img-fluid logo" alt="brand" />
@@ -40,7 +32,7 @@ function NavBar() {
         <Navbar.Toggle
           aria-controls="responsive-navbar-nav"
           onClick={() => {
-            updateExpanded(expand ? false : "expanded");
+            updateExpanded(expand ? false : 'expanded')
           }}
         >
           <span></span>
@@ -49,54 +41,39 @@ function NavBar() {
         </Navbar.Toggle>
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ms-auto" defaultActiveKey="#home">
+            <Nav.Item>
+              <Nav.Link as={Link} to="/about" onClick={() => updateExpanded(false)}>
+                <AiOutlineUser style={{ marginBottom: '2px' }} /> About
+              </Nav.Link>
+            </Nav.Item>
 
             <Nav.Item>
-              <Nav.Link
-                as={Link}
-                to="/about"
-                onClick={() => updateExpanded(false)}
-              >
-                <AiOutlineUser style={{ marginBottom: "2px" }} /> About
+              <Nav.Link as={Link} to="/project" onClick={() => updateExpanded(false)}>
+                <AiOutlineFundProjectionScreen style={{ marginBottom: '2px' }} /> Projects
+              </Nav.Link>
+            </Nav.Item>
+
+            <Nav.Item>
+              <Nav.Link as={Link} to="/resume" onClick={() => updateExpanded(false)}>
+                <CgFileDocument style={{ marginBottom: '2px' }} /> Resume
               </Nav.Link>
             </Nav.Item>
 
             <Nav.Item>
               <Nav.Link
-                as={Link}
-                to="/project"
+                as="a"
+                href="https://theminermind.hashnode.dev/"
+                target="_blank"
                 onClick={() => updateExpanded(false)}
               >
-                <AiOutlineFundProjectionScreen
-                  style={{ marginBottom: "2px" }}
-                />{" "}
-                Projects
-              </Nav.Link>
-            </Nav.Item>
-
-            <Nav.Item>
-              <Nav.Link
-                as={Link}
-                to="/resume"
-                onClick={() => updateExpanded(false)}
-              >
-                <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
-              </Nav.Link>
-            </Nav.Item>
-
-            <Nav.Item>
-              <Nav.Link
-                as={Link}
-                to="/blogs"
-                onClick={() => updateExpanded(false)}
-              >
-                <ImBlog style={{ marginBottom: "2px" }} /> Blogs
+                <ImBlog style={{ marginBottom: '2px' }} /> Blogs
               </Nav.Link>
             </Nav.Item>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
-  );
+  )
 }
 
-export default NavBar;
+export default NavBar
