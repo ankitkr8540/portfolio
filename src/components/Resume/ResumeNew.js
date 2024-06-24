a71eba8a91097b1be1d80f5d2a43e22e20cf1844
Ankit Kumar <ankitkr8540@gmail.com>
2024-06-24 13:10:43 -0400

    added switch for resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,56 +1,75 @@
-import React, { useState, useEffect } from "react";
-import { Container, Row } from "react-bootstrap";
-import Button from "react-bootstrap/Button";
-import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Ankit-Kumar-Resume.pdf";
-import { AiOutlineDownload } from "react-icons/ai";
-import { Document, Page, pdfjs } from "react-pdf";
-import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+import React, { useState, useEffect } from 'react'
+import { Container, Row, Tab, Tabs } from 'react-bootstrap'
+import Button from 'react-bootstrap/Button'
+import Particle from '../Particle'
+import { AiOutlineDownload } from 'react-icons/ai'
+import { Document, Page, pdfjs } from 'react-pdf'
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css'
+import pdfSoftware from '../../Assets/Project_Assistant_Resume_Ankit.pdf'
+import pdfDataScientist from '../../Assets/Ankit-Kumar_Sp_24.pdf'
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`
 
-function ResumeNew() {
-  const [width, setWidth] = useState(1200);
+const ResumeNew = () => {
+  const [width, setWidth] = useState(1200)
+  const [key, setKey] = useState('software')
 
   useEffect(() => {
-    setWidth(window.innerWidth);
-  }, []);
+    setWidth(window.innerWidth)
+  }, [])
 
   return (
     <div>
       <Container fluid className="resume-section">
         <Particle />
-        <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button
-            variant="primary"
-            href={pdf}
-            target="_blank"
-            style={{ maxWidth: "250px" }}
+        <Row style={{ justifyContent: 'center', position: 'relative' }}>
+          <Tabs
+            id="resume-tabs"
+            activeKey={key}
+            onSelect={(k) => setKey(k)}
+            className="mb-3 justify-content-center d-flex custom-tabs border-0"
           >
-            <AiOutlineDownload />
-            &nbsp;Download CV
-          </Button>
-        </Row>
+            <Tab eventKey="software" title="Software Development" className="border-0">
+              <Button variant="primary" href={pdfSoftware} target="_blank" style={{ maxWidth: '250px' }}>
+                <AiOutlineDownload />
+                &nbsp;Download Software Development CV
+              </Button>
 
-        <Row className="resume">
-          <Document file={pdf} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-          </Document>
-        </Row>
+              <Row className="resume">
+                <Document file={pdfSoftware} className="d-flex justify-content-center">
+                  <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+                </Document>
+              </Row>
+              <Row style={{ justifyContent: 'center', position: 'relative' }}>
+                <Button variant="primary" href={pdfSoftware} target="_blank" style={{ maxWidth: '250px' }}>
+                  <AiOutlineDownload />
+                  &nbsp;Download Software Development CV
+                </Button>
+              </Row>
+            </Tab>
+            <Tab eventKey="data" title="Data Scientist">
+              <Button variant="primary" href={pdfDataScientist} target="_blank" style={{ maxWidth: '250px' }}>
+                <AiOutlineDownload />
+                &nbsp;Download Data Scientist CV
+              </Button>
 
-        <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button
-            variant="primary"
-            href={pdf}
-            target="_blank"
-            style={{ maxWidth: "250px" }}
-          >
-            <AiOutlineDownload />
-            &nbsp;Download CV
-          </Button>
+              <Row className="resume">
+                <Document file={pdfDataScientist} className="d-flex justify-content-center">
+                  <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+                </Document>
+              </Row>
+
+              <Row style={{ justifyContent: 'center', position: 'relative' }}>
+                <Button variant="primary" href={pdfSoftware} target="_blank" style={{ maxWidth: '250px' }}>
+                  <AiOutlineDownload />
+                  &nbsp;Download Data Scientist CV
+                </Button>
+              </Row>
+            </Tab>
+          </Tabs>
         </Row>
       </Container>
     </div>
-  );
+  )
 }
 
-export default ResumeNew;
+export default ResumeNew
