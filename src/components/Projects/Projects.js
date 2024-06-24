6d301b896db9cb45a8d190a654543545c603ab7e
Ankit Kumar <ankitkr8540@gmail.com>
2024-06-23 20:13:26 -0400

    added leetcode-calendar project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,9 +1,10 @@
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import ProjectCard from "./ProjectCards";
-import Particle from "../Particle";
-import Basta from "../../Assets/Basta_preview.png"
-import clg from "../../Assets/cover-letter-generator.jpeg"
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+import ProjectCard from './ProjectCards'
+import Particle from '../Particle'
+import Basta from '../../Assets/Basta_preview.png'
+import clg from '../../Assets/cover-letter-generator.jpeg'
+import leetcode from '../../Assets/leetcode-previewed.jpeg'
 
 function Projects() {
   return (
@@ -13,16 +14,14 @@ function Projects() {
         <h1 className="project-heading">
           My Recent <strong className="purple">Works </strong>
         </h1>
-        <p style={{ color: "white" }}>
-          Here are a few projects I've worked on recently.
-        </p>
-        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+        <p style={{ color: 'white' }}>Here are a few projects I've worked on recently.</p>
+        <Row style={{ justifyContent: 'center', paddingBottom: '10px' }}>
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={Basta}
               isBlog={false}
-              title="Basta"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
+              title="Basta E-commerce Shopping App"
+              description="Basta is an E-commerce shopping app built using MERN stack. It has features like user authentication, product search, add to cart, and payment gateway integration. It is deployed on Heroku."
               ghLink="https://github.com/ankitkr8540/EcommerceShoppingApp"
               demoLink="https://basta-bag-26da25a17a75.herokuapp.com/"
             />
@@ -37,10 +36,20 @@ function Projects() {
               demoLink="https://better-coverletter-generator.streamlit.app/"
             />
           </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={leetcode}
+              isBlog={false}
+              title="Leetcode Calendar npm package"
+              description="It is an npm package that generates a calendar of Leetcode problems based on the user's preferences. It is built using Node.js and is published on npm."
+              ghLink="https://github.com/ankitkr8540/leetcode-calendar"
+              demoLink="https://www.npmjs.com/package/leetcode-calendar"
+            />
+          </Col>
         </Row>
       </Container>
     </Container>
-  );
+  )
 }
 
-export default Projects;
+export default Projects
