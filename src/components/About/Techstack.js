2c773029d06e09996e8d978fc2909d6318a6c291
Ankit Kumar <ankitkr8540@gmail.com>
2024-06-30 08:28:22 -0400

    made changes for the name of the techStack and tools

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -19,45 +19,59 @@ function Techstack() {
     <Row style={{ justifyContent: 'center', paddingBottom: '50px' }}>
       <Col xs={4} md={2} className="tech-icons" title="Python">
         <DiPython />
+        <div className="tech-name">Python</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons" title="Pandas">
         <SiPandas />
+        <div className="tech-name">Pandas</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons" title="Numpy">
         <SiNumpy />
+        <div className="tech-name">Numpy</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons" title="JavaScript">
         <DiJavascript1 />
+        <div className="tech-name">JavaScript</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons" title="Angular js">
         <SiAngularjs />
+        <div className="tech-name">Angular js</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons" title="React js">
         <DiReact />
+        <div className="tech-name">React js</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons" title="Ionic Framework">
         <DiIonic />
+        <div className="tech-name">Ionic Framework</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons" title="CSS">
         <FaCss3 />
+        <div className="tech-name">CSS</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons" title="MongoDB">
         <DiMongodb />
+        <div className="tech-name">MongoDB</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons" title="Node js">
         <DiNodejs />
+        <div className="tech-name">Node js</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons" title="Git">
         <DiGit />
+        <div className="tech-name">Git</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons" title="Firebase">
         <SiFirebase />
+        <div className="tech-name">Firebase</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons" title="streamlit">
         <SiStreamlit />
+        <div className="tech-name">streamlit</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons" title="Snowflake">
         <SiSnowflake />
+        <div className="tech-name">Snowflake</div>
       </Col>
     </Row>
   )
--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -9,21 +9,27 @@ function Toolstack() {
     <Row style={{ justifyContent: 'center', paddingBottom: '50px' }}>
       <Col xs={4} md={2} className="tech-icons" title="macOS">
         <SiMacos />
+        <div className="tech-name">macOS</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons" title="visual Studio">
         <SiVisualstudiocode />
+        <div className="tech-name">Visual Studio</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons" title="Python Notebook">
         <VscNotebook />
+        <div className="tech-name">Python Notebook</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons" title="Postman">
         <SiPostman />
+        <div className="tech-name">Postman</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons" title="Heroku">
         <SiHeroku />
+        <div className="tech-name">Heroku</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons" title="Github">
         <FaGithub />
+        <div className="tech-name">Github</div>
       </Col>
     </Row>
   )
