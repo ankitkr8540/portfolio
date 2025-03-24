9d4a540a38735401560cfc53d26b8b76e9162c0a
Ankit Kumar <ankitkr8540@gmail.com>
2025-03-23 23:26:07 -0400

    added my image on homepage and updated the resume width

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,76 +1,95 @@
-import React, { useState, useEffect } from 'react'
-import { Container, Row, Col, Tab, Tabs } from 'react-bootstrap'
-import Button from 'react-bootstrap/Button'
-import Particle from '../Particle'
-import { AiOutlineDownload } from 'react-icons/ai'
-import { FaChevronCircleLeft, FaChevronCircleRight } from 'react-icons/fa'
-import { Document, Page, pdfjs } from 'react-pdf'
-import 'react-pdf/dist/esm/Page/AnnotationLayer.css'
-import pdfDataScientist from '../../Assets/Ankit_MLE_Resume.pdf'
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`
+import React, { useState, useEffect } from 'react';
+import { Container, Row, Col, Tab, Tabs } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import Particle from '../Particle';
+import { AiOutlineDownload } from 'react-icons/ai';
+import { FaChevronCircleLeft, FaChevronCircleRight } from 'react-icons/fa';
+import { Document, Page, pdfjs } from 'react-pdf';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import pdfDataScientist from '../../Assets/Ankit_MLE_Resume.pdf';
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const ResumeNew = () => {
-  const [width, setWidth] = useState(1200)
-  const [key, setKey] = useState('software')
-  const [numPages, setNumPages] = useState(null)
-  const [pageNumber, setPageNumber] = useState(1)
+  const [width, setWidth] = useState(1200);
+  const [key, setKey] = useState('software');
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
 
   useEffect(() => {
-    setWidth(window.innerWidth)
-  }, [])
+    setWidth(window.innerWidth);
+  }, []);
 
   const onDocumentLoadSuccess = ({ numPages }) => {
-    setNumPages(numPages)
-  }
+    setNumPages(numPages);
+  };
 
   const changePage = (offset) => {
-    setPageNumber((prevPageNumber) => prevPageNumber + offset)
-  }
+    setPageNumber((prevPageNumber) => prevPageNumber + offset);
+  };
 
   const previousPage = () => {
-    changePage(-1)
-  }
+    changePage(-1);
+  };
 
   const nextPage = () => {
-    changePage(1)
-  }
+    changePage(1);
+  };
 
   return (
     <div>
       <Particle />
-      <Container className="resume-section">
+      <Container className='resume-section'>
         <Row style={{ justifyContent: 'center', position: 'relative' }}>
-          <Row className="resume">
+          <Row className='resume'>
             <Document
               onLoadSuccess={onDocumentLoadSuccess}
               file={pdfDataScientist}
               options={{ workerSrc: '/pdf.worker.js' }}
-              className="d-flex justify-content-center"
+              className='d-flex justify-content-center'
             >
-              <Page pageNumber={pageNumber} width={width > 786 ? 786 : width - 20} />
+              <Page
+                pageNumber={pageNumber}
+                width={width > 786 ? 1200 : width - 20}
+              />
             </Document>
           </Row>
-          <Row className="d-flex justify-content-center mt-3">
+          <Row className='d-flex justify-content-center mt-3'>
             <p>
               Page {pageNumber || (numPages ? 1 : '--')} of {numPages || '--'}
             </p>
           </Row>
-          <Row className="d-flex custom-tabs justify-content-center mb-3" style={{ width: 'auto' }}>
-            <Col xs="auto">
-              <Button onClick={previousPage} disabled={pageNumber <= 1} variant="outline-primary">
+          <Row
+            className='d-flex custom-tabs justify-content-center mb-3'
+            style={{ width: 'auto' }}
+          >
+            <Col xs='auto'>
+              <Button
+                onClick={previousPage}
+                disabled={pageNumber <= 1}
+                variant='outline-primary'
+              >
                 <FaChevronCircleLeft />
                 &nbsp;Previous
               </Button>
             </Col>
-            <Col xs="auto">
-              <Button onClick={nextPage} disabled={pageNumber >= numPages} variant="outline-primary">
+            <Col xs='auto'>
+              <Button
+                onClick={nextPage}
+                disabled={pageNumber >= numPages}
+                variant='outline-primary'
+              >
                 Next&nbsp;
                 <FaChevronCircleRight />
               </Button>
             </Col>
           </Row>
           <Row style={{ justifyContent: 'center', position: 'relative' }}>
-            <Button variant="primary" href={pdfDataScientist} target="_blank" style={{ maxWidth: '250px' }}>
+            <Button
+              variant='primary'
+              href={pdfDataScientist}
+              target='_blank'
+              style={{ maxWidth: '250px' }}
+            >
               <AiOutlineDownload />
               &nbsp;Download CV
             </Button>
@@ -78,7 +97,7 @@ const ResumeNew = () => {
         </Row>
       </Container>
     </div>
-  )
-}
+  );
+};
 
-export default ResumeNew
+export default ResumeNew;
